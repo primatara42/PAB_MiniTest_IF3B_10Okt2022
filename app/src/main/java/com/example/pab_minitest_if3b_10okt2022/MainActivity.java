b2319f218966d495eefe1d366be635fc4d35fc25
primatara42 <primatara42@gmail.com>
2022-10-10 11:09:52 +0700

    Commit

---
+++ app/src/main/java/com/example/pab_minitest_if3b_10okt2022/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.pab_minitest_if3b_10okt2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnButton = findViewById(R.id.btn_button);
+
+        btnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Apa Kabar Kamu", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
